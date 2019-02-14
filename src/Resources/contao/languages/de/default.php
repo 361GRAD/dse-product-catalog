8c082ee90e8a5ea7fa0faf40d2fcb47f5aac4d9b
markus361 <markus@361.de>
2019-02-14 10:35:27 +0100

    added translations

---
+++ src/Resources/contao/languages/de/default.php
@@ -0,0 +1,4 @@
+<?php 
+$GLOBALS['TL_LANG']['MSC']['IMPORT']['import_legend'] = 'Datenimport';
+$GLOBALS['TL_LANG']['MSC']['IMPORT']['import_file'] = array('Quelldatei', 'Wählen Sie eine CSV Datei');
+$GLOBALS['TL_LANG']['MSC']['IMPORT']['import_select'] = array('Kategorie', 'Auswahl der Kategorie in welche die Daten importiert werden.');
---
+++ src/Resources/contao/languages/en/default.php
@@ -0,0 +1,4 @@
+<?php 
+$GLOBALS['TL_LANG']['MSC']['IMPORT']['import_legend'] = 'Data import';
+$GLOBALS['TL_LANG']['MSC']['IMPORT']['import_file'] = array('Source file', 'Select a CSV file');
+$GLOBALS['TL_LANG']['MSC']['IMPORT']['import_select'] = array('Category', 'Select the category where the data should be imported');
