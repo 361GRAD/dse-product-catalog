5f4eb5aff7c1fc9efcd6ee8ed034dd9068cc516d
markus361 <markus@361.de>
2020-10-15 09:50:57 +0200

    added attributes

---
+++ src/Resources/contao/languages/fr/default.php
@@ -0,0 +1,5 @@
+<?php 
+$GLOBALS['TL_LANG']['MSC']['EXPORT']['attr_t_softschlag'] = 'Percussion douce (XIP) commutable';
+$GLOBALS['TL_LANG']['MSC']['EXPORT']['attr_t_bereich_diamantbohrkrone'] = 'Capacité de forage avec couronne, avec support BS, à sec';
+$GLOBALS['TL_LANG']['MSC']['EXPORT']['attr_t_bereich_beton_hand'] = 'Capacité de forage avec couronne, à main, à sec, avec percussion douce';
+$GLOBALS['TL_LANG']['MSC']['EXPORT']['attr_t_bereich_beton_staender'] = 'Capacité de forage avec couronne, avec support BS, à sec, avec percussion douce';
